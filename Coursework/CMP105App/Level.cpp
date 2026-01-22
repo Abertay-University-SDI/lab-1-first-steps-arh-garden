6a9bca0382a599414f5a1ce963e1254fc35c5de0
Arden Hunt <256573410+arh-garden@users.noreply.github.com>
2026-01-22 15:03:43 +0000

    add direction input

--- Coursework/CMP105App/Level.cpp
+++ Coursework/CMP105App/Level.cpp
@@ -1,5 +1,7 @@
 #include "Level.h"
 
+
+sf::Vector2f m_inputDirection;
 Level::Level(sf::RenderWindow& hwnd, Input& in) :
 	BaseLevel(hwnd, in)
 {
@@ -10,15 +12,22 @@ Level::Level(sf::RenderWindow& hwnd, Input& in) :
 // handle user input
 void Level::handleInput(float dt)
 {
-	if (m_input.isLeftMousePressed())
-	{
-		std::cout << "left mouse pressed" << std::endl;
+	// move input
+	m_inputDirection = { 0,0 };
+	// x
+	if (m_input.isKeyDown(sf::Keyboard::Scancode::W)) {
+		m_inputDirection += {0, -1};
 	}
-	if (m_input.isKeyDown(sf::Keyboard::Scan::A))
-	{
-		std::cout << "AYE!" << std::endl;
+	if (m_input.isKeyDown(sf::Keyboard::Scancode::S)) {
+		m_inputDirection += {0, 1};
 	}
-
+	// y
+	if (m_input.isKeyDown(sf::Keyboard::Scancode::D)) {
+		m_inputDirection += {1, 0};
+	}
+	if (m_input.isKeyDown(sf::Keyboard::Scancode::A)) {
+		m_inputDirection += {-1, 0};
+	}
 }
 
 // Update game objects
